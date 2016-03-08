e90870989dbe92d952e765c0a65a932ec76343e1
albert.chen <15296809123@163.com>
2016-03-08 17:43:56 +0800

    test

--- Practice/Test/Practice_exeercise.py
+++ Practice/Test/Practice_exeercise.py
@@ -1,6 +1,15 @@
-'''
-Created on Feb 2, 2016
+#!/usr/bin/env python
+#-*- coding: utf -*-
 
-@author: Administrator
-'''
-print "Hello"
+
+import copy
+
+seq = [1, 2, 3] 
+seq_1 = seq
+seq_2 = copy.copy(seq)
+seq_3 = copy.deepcopy(seq)
+seq.append(4) 
+
+print seq_2 
+print seq
+print seq_3
